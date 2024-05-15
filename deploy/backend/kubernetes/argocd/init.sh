da63f6e11536b661b85678dfee1f8c41d0da4e30
mandala <rcc@vip.qq.com>
2024-05-15 14:22:39 +0800

    test: add test pipeline

--- deploy/backend/kubernetes/argocd/init.sh
+++ deploy/backend/kubernetes/argocd/init.sh
@@ -197,59 +197,139 @@ spec: # 规范部分
         maxDuration: 3m  # 最大重试间隔
 EOF
 
-cat > project-app.yml <<EOF
-apiVersion: argoproj.io/v1alpha1
-kind: AppProject
-metadata:
-  name: ${FRONTEND_NAMESPACE}
-  namespace: ${ARGOCD_NAMESPACE}
-spec:
-  # 说明
-  description: Project for frontend applications
-  # 允许的集群
-  destinations:
-    # 允许的命名空间
-    - name: ${FRONTEND_PROJECT_NAME}
-      namespace: "${FRONTEND_NAMESPACE}"
-      # 集群地址
-      server: ${CLUSTER_SERVER}
-    - name: ${BACKEND_PROJECT_NAME}
-      namespace: "{BACKEND_NAMESPACE}"
-      # 集群地址
-      server: ${CLUSTER_SERVER}
-  # 允许的目标 K8s 资源类型
-  clusterResourceWhitelist:
-    - group: '*'
-      #kind: '*'
-      kind: Namespace
-  # Allow all namespaced-scoped resources to be created, except for ResourceQuota, LimitRange, NetworkPolicy
-  #namespaceResourceBlacklist:
-  #  - group: ''
-  #    kind: ResourceQuota
-  #  - group: ''
-  #    kind: LimitRange
-  #  - group: ''
-  #    kind: NetworkPolicy
-  # Deny all namespaced-scoped resources from being created, except for Deployment and StatefulSet
-  #namespaceResourceWhitelist:
-  #  - group: 'apps'
-  #    kind: Deployment
-  #  - group: 'apps'
-  #    kind: StatefulSet
-  sourceRepos:
-    - "*"
-  roles:
-    - name: ${ROLE_NAME}
-      description: Access role for ROLE_NAME user
-      policies:
-        - p, proj:default:${ROLE_NAME}, applications, *, ${FRONTEND_PROJECT_NAME}/*, allow
-        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, get, ${FRONTEND_PROJECT_NAME}/*, allow
-        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, create, ${FRONTEND_PROJECT_NAME}/*, allow
-        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, sync, ${FRONTEND_PROJECT_NAME}/*, allow
-        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, delete, ${FRONTEND_PROJECT_NAME}/*, allow
-        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, repositories, *, ${FRONTEND_PROJECT_NAME}/*, allow
-        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, clusters, get, ${FRONTEND_PROJECT_NAME}/*, allow
-  orphanedResources:
-    warn: true
-# kubectl apply -f project-app.yml
+cat > application-frontend.yml <<EOF
+apiVersion: argoproj.io/v1alpha1  # 指定 Argo CD API 版本
+kind: Application  # 定义资源类型为 Application
+metadata: # 元数据部分
+  name: ${FRONTEND_APPLICATION_NAME}   # 指定 Application 的名称
+  namespace: ${ARGOCD_NAMESPACE}  # argocd所属的命名空间
+  # 定义资源的 finalizers
+  # https://argo-cd.readthedocs.io/en/stable/user-guide/app_deletion/#about-the-deletion-finalizer
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io  # 删除时行级联删除
+    #- resources-finalizer.argocd.argoproj.io/background  # 删除时后台行级联删除
+spec: # 规范部分
+  project: ${FRONTEND_PROJECT_NAME}  # 应用程序将被配置的项目名称，这是在 Argo CD 中应用程序的一种组织方式
+  source: # 指定源
+    # Kubernetes 资源清单在仓库中的路径
+    path: ${FRONTEND_DEPLOY_PATH}
+    # 指定 Git 仓库的 URL
+    repoURL: ${PROJECT_GIT_URL}
+    # 使用的 git 分支
+    targetRevision: ${BRANCH}
+  # 部署应用到Kubernetes 集群中的位置
+  destination:
+    namespace: ${FRONTEND_NAMESPACE}  # 指定应用的命名空间
+    server: ${CLUSTER_SERVER}  # 如果部署到同一集群，可以省略
+  syncPolicy: # 指定同步策略
+    automated: # 自动化同步
+      prune: true  # 启用资源清理
+      selfHeal: true  # 启用自愈功能
+      allowEmpty: false  # 禁止空资源
+    syncOptions: # 同步选项
+      - Validate=false  # 是否启用验证
+      - CreateNamespace=true  # 启用创建命名空间
+    retry: # 重试策略
+      limit: 5  # 重试次数上限
+      backoff: # 重试间隔
+        duration: 10s  # 初始重试间隔
+        factor: 2  # 重试间隔因子
+        maxDuration: 3m  # 最大重试间隔
 EOF
+
+cat > application-backend.yml <<EOF
+apiVersion: argoproj.io/v1alpha1  # 指定 Argo CD API 版本
+kind: Application  # 定义资源类型为 Application
+metadata: # 元数据部分
+  name: ${BACKEND_APPLICATION_NAME}   # 指定 Application 的名称
+  namespace: ${ARGOCD_NAMESPACE}  # argocd所属的命名空间
+  # 定义资源的 finalizers
+  # https://argo-cd.readthedocs.io/en/stable/user-guide/app_deletion/#about-the-deletion-finalizer
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io  # 删除时行级联删除
+    #- resources-finalizer.argocd.argoproj.io/background  # 删除时后台行级联删除
+spec: # 规范部分
+  project: ${BACKEND_PROJECT_NAME}  # 应用程序将被配置的项目名称，这是在 Argo CD 中应用程序的一种组织方式
+  source: # 指定源
+    # Kubernetes 资源清单在仓库中的路径
+    path: ${BACKEND_DEPLOY_PATH}
+    # 指定 Git 仓库的 URL
+    repoURL: ${PROJECT_GIT_URL}
+    # 使用的 git 分支
+    targetRevision: ${BRANCH}
+  # 部署应用到Kubernetes 集群中的位置
+  destination:
+    namespace: ${BACKEND_NAMESPACE}  # 指定应用的命名空间
+    server: ${CLUSTER_SERVER}  # 如果部署到同一集群，可以省略
+  syncPolicy: # 指定同步策略
+    automated: # 自动化同步
+      prune: true  # 启用资源清理
+      selfHeal: true  # 启用自愈功能
+      allowEmpty: false  # 禁止空资源
+    syncOptions: # 同步选项
+      - Validate=false  # 是否启用验证
+      - CreateNamespace=true  # 启用创建命名空间
+    retry: # 重试策略
+      limit: 5  # 重试次数上限
+      backoff: # 重试间隔
+        duration: 10s  # 初始重试间隔
+        factor: 2  # 重试间隔因子
+        maxDuration: 3m  # 最大重试间隔
+EOF
+
+#cat > project-app.yml <<EOF
+#apiVersion: argoproj.io/v1alpha1
+#kind: AppProject
+#metadata:
+#  name: ${FRONTEND_NAMESPACE}
+#  namespace: ${ARGOCD_NAMESPACE}
+#spec:
+#  # 说明
+#  description: Project for frontend applications
+#  # 允许的集群
+#  destinations:
+#    # 允许的命名空间
+#    - name: ${FRONTEND_PROJECT_NAME}
+#      namespace: "${FRONTEND_NAMESPACE}"
+#      # 集群地址
+#      server: ${CLUSTER_SERVER}
+#    - name: ${BACKEND_PROJECT_NAME}
+#      namespace: "{BACKEND_NAMESPACE}"
+#      # 集群地址
+#      server: ${CLUSTER_SERVER}
+#  # 允许的目标 K8s 资源类型
+#  clusterResourceWhitelist:
+#    - group: '*'
+#      #kind: '*'
+#      kind: Namespace
+#  # Allow all namespaced-scoped resources to be created, except for ResourceQuota, LimitRange, NetworkPolicy
+#  #namespaceResourceBlacklist:
+#  #  - group: ''
+#  #    kind: ResourceQuota
+#  #  - group: ''
+#  #    kind: LimitRange
+#  #  - group: ''
+#  #    kind: NetworkPolicy
+#  # Deny all namespaced-scoped resources from being created, except for Deployment and StatefulSet
+#  #namespaceResourceWhitelist:
+#  #  - group: 'apps'
+#  #    kind: Deployment
+#  #  - group: 'apps'
+#  #    kind: StatefulSet
+#  sourceRepos:
+#    - "*"
+#  roles:
+#    - name: ${ROLE_NAME}
+#      description: Access role for ROLE_NAME user
+#      policies:
+#        - p, proj:default:${ROLE_NAME}, applications, *, ${FRONTEND_PROJECT_NAME}/*, allow
+#        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, get, ${FRONTEND_PROJECT_NAME}/*, allow
+#        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, create, ${FRONTEND_PROJECT_NAME}/*, allow
+#        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, sync, ${FRONTEND_PROJECT_NAME}/*, allow
+#        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, applications, delete, ${FRONTEND_PROJECT_NAME}/*, allow
+#        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, repositories, *, ${FRONTEND_PROJECT_NAME}/*, allow
+#        - p, proj:${FRONTEND_PROJECT_NAME}:${ROLE_NAME}, clusters, get, ${FRONTEND_PROJECT_NAME}/*, allow
+#  orphanedResources:
+#    warn: true
+## kubectl apply -f project-app.yml
+#EOF
