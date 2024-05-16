d91e5fabdacac10790da47d3a07e2a7c462f005e
mandala <rcc@vip.qq.com>
2024-05-16 20:02:01 +0800

    test: add test pipeline

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,60 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+/**
+ * @description
+ * @param command { "build" | "server" } 模式
+ * @param mode { string } 模式
+ * @param isSsrBuild { string } 是否为SSR 构建
+ * @param isPreview { string } 是否为正在预览的构建产物
+ * @returns
+ */
+export default defineConfig(async ({command}) => {
+	if (command === 'serve') {
+		return {
+			// dev 独有配置
+			plugins: [
+				react(),
+				basicSsl({
+					/** name of certification */
+					name: 'test',
+					/** custom trust domains */
+					domains: ['*.custom.com'],
+					/** custom certification directory */
+					certDir: '/Users/.../.devServer/cert',
+				}),
+			],
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
-	plugins: [react()],
-
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	//
-	// 1. prevent vite from obscuring rust errors
-	clearScreen: false,
-	// 2. tauri expects a fixed port, fail if that port is not available
-	server: {
-		port: 1420,
-		strictPort: true,
-		watch: {
-			// 3. tell vite to ignore watching `src-tauri`
-			ignored: ['**/src-tauri/**'],
-		},
-	},
-}))
+			// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+			//
+			// 1. prevent vite from obscuring rust errors
+			clearScreen: false,
+			// 2. tauri expects a fixed port, fail if that port is not available
+			server: {
+				host: '0.0.0.0',
+				port: 3000,
+				strictPort: true,
+				watch: {
+					// 3. tell vite to ignore watching `src-tauri`
+					ignored: ['**/src-tauri/**'],
+				},
+			},
+		}
+	} else {
+		return {
+			// build 独有配置
+			plugins: [react()],
+			// 开发或生产环境服务的公共基础路径。合法的值包括以下几种：
+			// 绝对 URL 路径名，例如 /foo/
+			// 完整的 URL，例如 https://foo.com/（原始的部分在开发环境中不会被使用）
+			// 空字符串或 ./（用于嵌入形式的开发）
+			base: './',
+			mode: 'production',
+			server: {
+				host: '0.0.0.0',
+				port: 443,
+				strictPort: true,
+			},
+		}
+	}
+})
